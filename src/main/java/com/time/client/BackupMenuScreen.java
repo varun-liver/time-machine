b8bc0c4aad3761c7d8092240cd2efc03817dc539
Varun <varun.sureshkumar@outlook.com>
2026-05-25 18:33:09 -0700

    hi

--- src/main/java/com/time/client/BackupMenuScreen.java
+++ src/main/java/com/time/client/BackupMenuScreen.java
@@ -145,7 +145,7 @@ public class BackupMenuScreen extends Screen {
     private void restoreAndReloadWorld(Minecraft minecraft, MinecraftServer server, Path worldFolder, String worldId, Path backupDirectory) {
         try {
             minecraft.execute(() -> {
-                minecraft.disconnect(createProgressScreen("Unloading world..."));
+                minecraft.disconnect(createProgressScreen("Unloading world..."), false);
             });
             waitForWorldUnload(minecraft, 10000L);
 
